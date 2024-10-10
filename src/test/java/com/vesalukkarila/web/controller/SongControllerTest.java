13bb84d41867e064190fdbba78c3e696d1ef90af
Vesa Lukkarila <github@vesalukkarila.com>
2024-10-10 17:57:19 +0300

    Add: tests for getSongs in a nested test class

--- src/test/java/com/vesalukkarila/web/controller/SongControllerTest.java
+++ src/test/java/com/vesalukkarila/web/controller/SongControllerTest.java
@@ -3,11 +3,13 @@ package com.vesalukkarila.web.controller;
 import com.vesalukkarila.model.Song;
 import com.vesalukkarila.service.SongService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -33,12 +35,37 @@ public class SongControllerTest {
         assertEquals(response.get("message"), "Hello from Songs API");
     }
 
-    @Test
-    public void testGetSongs(){
-        List<Song> songs = List.of(new Song("Name", "Artist", 1985));
-        when(songService.findAll()).thenReturn(songs);
-        List<Song> response = songsController.getSongs();
-        assertEquals(1, response.size());
-        assertEquals("Name", response.get(0).getName());
+    @Nested
+    public class testGetSongs{
+
+        @Test
+        public void testWithZeroSongs(){
+            List<Song> songs = new ArrayList<>();
+            when(songService.findAll()).thenReturn(songs);
+            List<Song> response = songsController.getSongs();
+            assertEquals(0, response.size());
+        }
+
+        @Test
+        public void testWithOneSong(){
+            List<Song> songs = List.of(
+                    new Song("The Thrill Is Gone", "B.B. King", 1969));
+            when(songService.findAll()).thenReturn(songs);
+            List<Song> response = songsController.getSongs();
+            assertEquals(1, response.size());
+            assertEquals("The Thrill Is Gone", response.get(0).getName());
+        }
+
+        @Test
+        public void testWithTwoSongs(){
+            List<Song> songs = Arrays.asList(
+                    new Song("The Thrill Is Gone", "B.B. King", 1969),
+                    new Song("Sweet Home Chicago", "Robert Johnson", 1936));
+            when(songService.findAll()).thenReturn(songs);
+            List<Song> response = songsController.getSongs();
+            assertEquals(2, response.size());
+            assertEquals("The Thrill Is Gone", response.get(0).getName());
+            assertEquals("Sweet Home Chicago", response.get(1).getName());
+        }
     }
 }
