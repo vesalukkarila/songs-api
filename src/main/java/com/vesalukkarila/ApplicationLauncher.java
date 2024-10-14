7c4e0b740fe241ca50dd2d8250136fb25ab1b2ce
Vesa Lukkarila <github@vesalukkarila.com>
2024-10-14 17:05:32 +0300

    add javadoc

--- src/main/java/com/vesalukkarila/ApplicationLauncher.java
+++ src/main/java/com/vesalukkarila/ApplicationLauncher.java
@@ -10,18 +10,33 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+/**
+ * The entry point of the Spring application that runs on an embedded Tomcat server.
+ * This class initializes and starts the Tomcat server, sets up the Spring
+ * application context, and configures the DispatcherServlet to handle
+ * incoming requests.
+ */
 public class ApplicationLauncher {
 
-    public static void main(String[] args) throws LifecycleException{
+    /**
+     * The main method to launch the application.
+     *
+     * @param args command-line arguments (not used)
+     * @throws LifecycleException if the Tomcat server encounters a lifecycle error during startup
+     */
+    public static void main(String[] args) throws LifecycleException {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
         tomcat.getConnector();
         Context tomcatCtx = tomcat.addContext("", null);
 
+        // Create and configure the Spring application context
         WebApplicationContext appCtx = createApplicationContext(tomcatCtx.getServletContext());
 
+        // Initialize the DispatcherServlet
         DispatcherServlet dispatcherServlet = new DispatcherServlet(appCtx);
 
+        // Add the DispatcherServlet to the Tomcat context
         Wrapper servlet = Tomcat.addServlet(tomcatCtx, "dispatcherServlet", dispatcherServlet);
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/*");
@@ -29,7 +44,13 @@ public class ApplicationLauncher {
         tomcat.start();
     }
 
-    private static WebApplicationContext createApplicationContext(ServletContext servletContext){
+    /**
+     * Creates and configures a Spring WebApplicationContext for the application.
+     *
+     * @param servletContext the servlet context used to create the Spring application context
+     * @return the configured WebApplicationContext
+     */
+    private static WebApplicationContext createApplicationContext(ServletContext servletContext) {
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.register(ApplicationConfiguration.class);
         ctx.setServletContext(servletContext);
