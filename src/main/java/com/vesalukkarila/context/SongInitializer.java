20d3dd28aadaa80bde08c1bf01d03a2aec56de07
Vesa Lukkarila <github@vesalukkarila.com>
2024-10-14 16:21:23 +0300

    add javadoc

--- src/main/java/com/vesalukkarila/context/SongInitializer.java
+++ src/main/java/com/vesalukkarila/context/SongInitializer.java
@@ -6,15 +6,32 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+/**
+ * SongInitializer is responsible for initializing the application with default song data
+ * when the Spring application context is refreshed.
+ * This component listens for the ContextRefreshedEvent and checks if the song
+ * repository is empty. If it is empty, it adds a default song entry.
+ */
 @Component
 public class SongInitializer implements ApplicationListener<ContextRefreshedEvent> {
     private final SongService songService;
 
+    /**
+     * Constructs a new SongInitializer with the specified SongService.
+     *
+     * @param songService the SongService to be used for creating default songs
+     */
     public SongInitializer(SongService songService) {
         this.songService = songService;
     }
 
-
+    /**
+     * Invoked when the application context is refreshed.
+     * This method checks if there are any songs in the database. If no songs exist,
+     * it creates a default song with the title "Thunderstruck" by AC/DC, released in 1990.
+     *
+     * @param event the event containing the context refresh information
+     */
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         if (songService.findAll().isEmpty()) {
